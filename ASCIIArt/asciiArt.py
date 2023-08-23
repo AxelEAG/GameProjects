1f2d6d4de75ab36ae57cc52b03b7e95a52167f86
Axel Almaguer <axel.0619@hotmail.com>
2023-08-23 18:39:38 -0400

    Update asciiArt.py

--- ASCIIArt/asciiArt.py
+++ ASCIIArt/asciiArt.py
@@ -2,14 +2,13 @@ from PIL import Image as img
 
 image = img.open("ascii-pineapple.jpg")
 width, height = image.size
-print("Successfully loaded image!")
-print(f"Image size: {width} x {height}")
+# print("Successfully loaded image!")
+# print(f"Image size: {width} x {height}")
 image.resize((int(width*0.6), int(height*0.6)))
 image.show()
 width, height = image.size
 
 scale = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
-print(len(scale))
 pixels = list(image.getdata())
 new_pixels = []
 ascii_img = []
@@ -26,16 +25,8 @@ for n in range(len(pixels)):
         ascii_img.append(line)
         line = []
 
-print(len(ascii_img[0]))
 for i in range(len(ascii_img)):
     print(*ascii_img[i], sep="")
-
-
-# red, green, blue = image.split()
-# zeroed_band = red.point(lambda _: 0)
-#
-# red_merge = img.merge("RGB", (red, zeroed_band, zeroed_band))
-# red_merge.show()
 
 
 

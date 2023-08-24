e96ddb5ad3466bb352aa57bafd21ae1bdd18fd00
Axel Almaguer <axel.0619@hotmail.com>
2023-08-24 14:59:40 -0400

    Update asciiArt.py

--- ASCIIArt/asciiArt.py
+++ ASCIIArt/asciiArt.py
@@ -2,8 +2,6 @@ from PIL import Image as img
 
 image = img.open("ascii-pineapple.jpg")
 width, height = image.size
-# print("Successfully loaded image!")
-# print(f"Image size: {width} x {height}")
 image = image.resize((int(width*0.5), int(height*0.5)))
 width, height = image.size
 
@@ -14,10 +12,18 @@ ascii_img = []
 line = []
 
 for n in range(len(pixels)):
-    brightness = sum(pixels[n])/3
-    new_pixels.append(brightness)
+    # brightness = sum(pixels[n])/3
+    # new_pixels.append(brightness)
+    # num = int(65*brightness/255)
 
-    num = int(65*brightness/255)
+    # R, G, B = pixels[n]
+    # lightness = (max(R, G, B) + min(R, G, B))/2
+    # num = int(65 * lightness / 255) - 1
+
+    R, G, B = pixels[n]
+    lumosity = 0.21*R + 0.72*G + 0.07*B
+    num = int(65*lumosity/255) - 1
+    
     ascii_eq = scale[num]
     line.append(ascii_eq*3)
     if (n + 1) % width == 0:
